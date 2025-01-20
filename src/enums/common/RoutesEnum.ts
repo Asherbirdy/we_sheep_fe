2c27b6844990877f2554ff448a8ec7a1f8c89836
Asher Lien <birdym.tw@gmail.com>
2025-01-20 10:56:33 +0800

    更新

--- src/enums/common/RoutesEnum.ts
+++ src/enums/common/RoutesEnum.ts
@@ -1,12 +1,15 @@
+const client = 'C'
+const memberShip = 'M'
+
 export enum RoutesPath {
   // Public Routes
   PublicHome = '/',
 
   // Client Routes
-  ClientHome = '/C/',
-  ClientSecondPage = '/C/pageTwo',
+  ClientHome = `${client}/`,
+  ClientSecondPage = `${client}/pageTwo`,
 
   // MemberShip Routes
-  MemberShipHome = '/M/',
-  MemberShipSecondPage = '/M/pageTwo',
+  MemberShipHome = `${memberShip}/`,
+  MemberShipSecondPage = `${memberShip}/pageTwo`,
 }
