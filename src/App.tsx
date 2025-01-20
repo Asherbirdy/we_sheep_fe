75ce6236ef594e550877fa635a3131a7a9ac65fe
Asher Lien <birdym.tw@gmail.com>
2025-01-20 11:36:19 +0800

    引入quert

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,22 @@ import { ColorModeScript } from '@chakra-ui/react'
 import React from 'react'
 import theme from './theme'
 import { ChakraProvider } from '@chakra-ui/react'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient({ defaultOptions: { queries: { staleTime: 1000 * 60 * 10  } } })
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <ColorModeScript />
-      <BrowserRouter>
-        <Routing />
-      </BrowserRouter>
-    </ChakraProvider>
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider theme={theme}>
+        <ColorModeScript />
+        <BrowserRouter>
+          <Routing />
+        </BrowserRouter>
+      </ChakraProvider>
+    </QueryClientProvider>
   </React.StrictMode>,
 )
